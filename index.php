463f313ce52866f7b178cd0f6c2e5d7b94be39d9
hamishgray <hamish.gray@secretescapes.com>
2019-04-05 10:09:15 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,127 @@
+<?php include "search.php" ?>
+
+<html lang="en">
+  <head>
+    <title>Custom Search | Secret Escapes</title>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i,700,700i|Source+Serif+Pro:400,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <div class="content-spacing section-spacing">
+
+
+      <!-- Form & sidebar -->
+      <div class="sidebar boxpad--xxl text">
+        <h1 class="title title--lg">Custom Search</h1>
+        <hr class="hr--left">
+        <div>
+          <p class="text text--xl">Enter your search terms below to test results. Use commas to separate keywords.</p>
+          <p class="text text--xl">The fields are constructive, so if you use keywords "london", "rome" and "paris", you will get results from all three cities. The tags field then filters upon this selection for example using "spa" will give you only results from the above three cities tagged with "spa".</p>
+          <div class="space--md"></div>
+          <form name="search" id="searchForm" onsubmit="searchForm()">
+
+            <label for='territory'>Territory:</label>
+            <select name="territory" id="territory">
+              <option value="uk">UK</option>
+              <option value="it">IT</option>
+              <option value="se">SE</option>
+            </select>
+            <div class="space--xs"></div>
+
+            <label for='keywords'>Keywords (e.g. Location, hotel name):</label>
+            <?php
+              if( searchQuery()['keywords'] ) {
+                echo "<input type='text' id='keywords' name='keywords' value='" . searchQuery()['keywords'] . "'>";
+              }else{
+                echo "<input type='text' id='keywords' name='keywords'>";
+              }
+            ?>
+            <div class="space--xs"></div>
+
+            <label for='tags'>Tags (e.g. CMS tags):</label>
+            <?php
+              if( searchQuery()['tags'] ) {
+                echo "<input type='text' id='tags' name='tags' value='" . searchQuery()['tags'] . "'>";
+              }else{
+                echo "<input type='text' id='tags' name='tags'>";
+              }
+            ?>
+
+            <div class="space--sm"></div>
+            <input type="submit" class="btn btn--orange">
+            <a href="javascript:resetForm()" class="btn btn--subtle">Reset</a>
+          </form>
+        </div>
+      </div>
+
+
+      <!-- Search results -->
+      <div class="search-results boxpad--xxl">
+
+        <div class="search-results__title">
+          <h2>
+          <?php
+            if( searchQuery() ) {
+              echo "<span class='title'>Your search: </span>";
+              echo search()['total'];
+              echo " results";
+            }else{
+              echo "<span class='title'>Showing all sales</span>";
+            }
+          ?>
+          </h2>
+
+          <?php
+            if( searchQuery()['keywords'] ) {
+              echo "<p class='text--xl'><span class='text--bold'>Keywords: </span>" . searchQuery()['keywords'] . "</p>";
+            }
+            if( searchQuery()['tags'] ) {
+              echo "<p class='text--xl'><span class='text--bold'>Tags: </span>" . searchQuery()['tags'] . "</p>";
+            }
+          ?>
+        </div>
+
+        <?php
+          echo search()['results'];
+        ?>
+      </div>
+
+    </div>
+
+    <script type="text/javascript">
+
+      // Retain territory choice on reload
+      const urlParams = new URLSearchParams(window.location.search);
+      const territoryQ = urlParams.get('territory');
+      if(territoryQ){
+        document.getElementById('territory').value = territoryQ;
+      }else{
+        document.getElementById('territory').value = 'uk';
+      }
+
+      function searchForm(){
+        var curr_url = window.location.href;
+        var url = curr_url.substring(0, curr_url.indexOf('?'));
+
+        var territory = document.getElementById("territory");
+        var keywords = document.getElementById("keywords");
+        var tags = document.getElementById("tags");
+        url += '?territory=' + territory.value;
+        if( keywords.value && tags.value ){
+          url += '&keywords=' + keywords.value + '&tags=' + tags.value;
+        }else if(keywords.value){
+          url += '&keywords=' + keywords.value;
+        }else if(tags.value){
+          url += '&tags=' + tags.value;
+        }
+        window.location.href = url;
+      }
+      function resetForm(){
+        var url = window.location.href;
+        var clean_url = url.substring(0, url.indexOf('?'));
+        window.location.href = clean_url;
+      }
+    </script>
+  </body>
+</html>
---
+++ search.php
@@ -0,0 +1,155 @@
+<?php
+
+
+
+function search(){
+  /* ============================================
+   * API Connection and config
+   */
+  $territory = "uk";
+  $territoryQ = isset($_GET['territory']);
+  if($territoryQ){
+    $territory = $_GET['territory'];
+  }
+  $seapitoken = "90370f0a-cc20-46a7-9934-a1cc4df00502";
+  $saleDataURL = "https://api.secretescapes.com/v3/sales?se-api-token=".$seapitoken."&territory=".$territory;
+
+  $json = file_get_contents($saleDataURL);
+  $sales = json_decode($json, true);
+  // print_r($sales);
+
+
+
+  $outputArray = [];
+  /* ============================================
+   * KEYWORD FILTERING
+   */
+  $keywordsQ = isset($_GET['keywords']);
+  if($keywordsQ AND strlen($_GET['keywords']) >= 1 ){
+    $filterKeywords = $_GET['keywords'];
+    // separate keywords by comma separation
+    $keywordFilterArray = explode(', ', $filterKeywords);
+
+    // FILTERS - Checking set fields against filter
+    $mergedArray = [];
+    foreach($keywordFilterArray as $filter){
+      $filteredArray = array_filter($sales, function($sales) use($filter){
+        if( stripos( $sales["location"]["displayName"], $filter ) ){
+          return true;
+        }else if( stripos(" ".$sales["location"]["city"]["name"], $filter ) ){
+          return true;
+        }else if( stripos(" ".$sales["location"]["country"]["name"], $filter ) ){
+          return true;
+        }else if( stripos(" ".$sales["reasonToLove"], $filter ) ){
+          return true;
+        }else if( stripos(" ".$sales["title"], $filter ) ){
+          return true;
+        }
+      });
+      array_push($mergedArray, $filteredArray);
+    }
+    $mergedArray = call_user_func_array('array_merge', $mergedArray);
+    // print_r($mergedArray);
+    $outputArray = array_values($mergedArray); // reset array key values
+  }
+
+
+
+  /* ============================================
+   * TAG FILTERING
+   */
+
+  $filterByTags = $sales;
+  if( sizeof($outputArray) > 0 ){
+    $filterByTags = $outputArray;
+  }
+
+  $tagsQ = isset($_GET['tags']);
+  if( $tagsQ AND strlen($_GET['tags']) >= 1 ){
+    $filterTags = $_GET['tags'];
+
+    // separate tags by comma separation
+    $tagFilterArray = explode(', ', $filterTags);
+
+    // FILTERS - Checking set fields against filter
+    $mergedArray = [];
+    foreach($tagFilterArray as $filter){
+      $filteredArray = array_filter($filterByTags, function($filterByTags) use($filter){
+
+        for($i = 0; $i <= count($filterByTags["tags"])-1; $i++){
+          $tag = $filterByTags["tags"][$i];
+          if( stripos( " ".$tag["key"], $filter ) ){
+            return true;
+          }
+        }
+      });
+      array_push($mergedArray, $filteredArray);
+    }
+    $mergedArray = call_user_func_array('array_merge', $mergedArray);
+    // print_r($mergedArray);
+    $outputArray = array_values($mergedArray); // reset array key values
+
+  }
+
+  if( !sizeof($outputArray) > 0 ){
+    $outputArray = $sales;
+  }
+
+  $filteredSales = $outputArray;
+  /* ============================================
+   * DISPLAY RESULTS
+   */
+
+  $total = sizeof($filteredSales);
+  $results = '';
+  for($i = 0; $i <= count($filteredSales)-1; $i++){
+    $item = $filteredSales[$i];
+    $tags = '';
+    for($i2 = 0; $i2 <= count($item["tags"])-1; $i2++){
+      $tags .= $item["tags"][$i2]["key"] . ", ";
+    }
+    $results.= "<div class='sale'>
+        <p>Title: ".$item['title']."</p>
+        <p>Location: ".$item['location']['displayName']."</p>
+        <p>City: ".$item['location']['city']['name']."</p>
+        <p>Country: ".$item['location']['country']['name']."</p>
+        <p>Reason to love: ".$item['reasonToLove']."</p>
+        <p>Tags: ".$tags."</p>
+        <p><a href='https://www.secretescapes.com/".$item['urlSlug']."/sale'>View sale</a></p>
+      </div>";
+    $tags=''; // reset tags for each sale
+  }
+
+  return array('results' => $results, 'total' => $total);
+
+}
+
+
+
+/* ============================================
+ * Display search query on page
+ */
+function searchQuery(){
+  $keywordsQ = isset($_GET['keywords']);
+  $tagsQ = isset($_GET['tags']);
+  $keywords = '';
+  $tags = '';
+
+  if($keywordsQ AND $tagsQ){
+    $keywords = $_GET['keywords'];
+    $tags = $_GET['tags'];
+    return array('keywords' => $keywords, 'tags' => $tags);
+
+  }else if($keywordsQ){
+    $keywords = $_GET['keywords'];
+    return array('keywords' => $keywords, 'tags' => $tags);
+
+  }else if($tagsQ){
+    $tags = $_GET['tags'];
+    return array('keywords' => $keywords, 'tags' => $tags);
+
+  }else{
+    return false;
+  }
+
+}
